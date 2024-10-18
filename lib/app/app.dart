044ee526e6d18f4f8f7751558acfd9f9d3f826b4
rozoomcool <rozoom.um@gmail.com>
2024-10-18 22:44:29 +0300

    setting theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:chguburoapp/domain/state/auth/auth_cubit.dart';
 import 'package:chguburoapp/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -18,6 +19,16 @@ class _MainAppState extends State<MainApp> {
     return MultiBlocProvider(
       providers: [BlocProvider(create: (context) => AuthCubit())],
       child: MaterialApp.router(
+        theme: ThemeData(
+          textTheme: GoogleFonts.montserratTextTheme(),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+          useMaterial3: true,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
+            )
+          )
+        ),
         routerConfig: _appRouter.config(),
       ),
     );
